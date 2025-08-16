738a24719db5aaef9e2d39db7a18396333337366
Jason Chambers <jason.d.chambers@gmail.com>
2025-08-16 16:11:04 -0600

    Installer script drafted

--- install.sh
+++ install.sh
@@ -1,3 +1,35 @@
 #!/usr/bin/env sh
 
-echo "TODO"
+point_tmux_to_this_config() {
+  config_file=$1
+  echo "Pointing tmux to this config"
+  if [ -L ~/.tmux.conf ]; then
+    echo "tmux config already set - verifying it points to this config"
+    local actual_resolved_path
+    actual_resolved_path=$(readlink -f ~/.tmux.conf)
+    if [ "$actual_resolved_path" = "$(pwd)/${config_file}" ]; then
+      echo "tmux is already pointing to this config"
+    else
+      echo "tmux is pointing to another config: ${actual_resolved_path}"
+      exit 1
+    fi
+  else
+    if [ -f ~/.tmux.conf ]; then
+      echo "tmux config already exists - moving it to backup"
+      mv ~/.tmux.conf{,.bak}
+    fi
+    echo "Linking tmux to this config"
+    ln -s $(pwd)/${config_file} ~/.tmux.conf
+  fi
+}
+
+main() {
+
+  if [ -e tmux.conf ]; then
+    point_tmux_to_this_config tmux.conf
+  else
+    echo "You need to run this script from within the config-tmux directory"
+  fi
+}
+
+main
